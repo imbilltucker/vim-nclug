3bd331946add0cf96bafad6832c0580342e967ec
Bill Tucker <bbtucker@us.ibm.com>
2013-09-10 14:06:44 -0600

    initial commit of sample files for use in vim exercies

---
+++ variables.js
@@ -0,0 +1,25 @@
+//Tasks:
+//Append a semicolon * 20
+//Comment out the short names
+
+    var foo = 1 
+    var bar = 'a' 
+    var baz = 'z' 
+    var bosh = 'o' 
+    var bish = 'i' 
+    var foobar = foo + bar 
+    var foobaz = foo + baz 
+    var foobosh = foo + bosh
+    var foobish = foo + bish
+    var barbar = bar + bar 
+    var barbaz = bar + baz 
+    var barbosh = bar + bosh
+    var barbish = bar + bish
+    var bazbar = baz + bar 
+    var bazbaz = baz + baz 
+    var bazbosh = baz + bosh
+    var bazbish = baz + bish
+    var foobarfoo = foobar + foo 
+    var foobarbar = foobar + bar 
+    var foobarbar = foobar + bar 
+
